6e33110d2fe5d50d98344850ce2a386d1c476ea1
michalovsky <michalovskyyy@gmail.com>
2020-02-20 13:00:54 +0100

    add tests for WordDescriptionGenerator

--- src/glossary/UT/Source/WordDescriptionGeneratorImplTest.cpp
+++ src/glossary/UT/Source/WordDescriptionGeneratorImplTest.cpp
@@ -1,7 +1,6 @@
 #include "WordDescriptionGeneratorImpl.h"
 
 #include "WordDescriptionServiceMock.h"
-#include "gmock/gmock.h"
 
 #include "gtest/gtest.h"
 
@@ -10,20 +9,72 @@ using namespace wordsDescriptionsDb;
 
 namespace
 {
-const EnglishWord englishWord{"englishWord"};
-const WordDescription wordDescription{englishWord, {}};
+const EnglishWord englishWord1{"englishWord1"};
+const EnglishWord englishWord2{"englishWord2"};
+const EnglishWord englishWord3{"englishWord3"};
+const EnglishWords englishWords{englishWord1, englishWord2, englishWord3};
+const WordDescription wordDescription1{englishWord1, Description{}};
+const WordDescription wordDescription2{
+    englishWord1,
+    Description{DefinitionsWithExamples{DefinitionWithExample{
+                    Definition{"definition"}, Example{"example"}}},
+                {}}};
+const WordDescription wordDescription3{englishWord3, Description{}};
+const boost::optional<WordDescription> wordDescription1Opt{wordDescription1};
+const boost::optional<WordDescription> wordDescription2Opt{wordDescription2};
+const WordsDescriptions wordsDescriptions{wordDescription1, wordDescription2, wordDescription3};
 }
 
-class WordDescriptionServiceImplTest : public Test
+class WordDescriptionGeneratorImplTest : public Test
 {
 public:
-    std::unique_ptr<HttpWordDescriptionCreatorMock> wordDescriptionCreatorInit =
-        std::make_unique<StrictMock<HttpWordDescriptionCreatorMock>>();
-    HttpWordDescriptionCreatorMock* wordDescriptionCreator =
-        wordDescriptionCreatorInit.get();
-    std::shared_ptr<wordsDescriptionsDb::WordsDescriptionsDbMock>
-        wordsDescriptionsDb = std::make_shared<
-            StrictMock<wordsDescriptionsDb::WordsDescriptionsDbMock>>();
-    WordDescriptionServiceImpl wordDescriptionService{
-        std::move(wordDescriptionCreatorInit), wordsDescriptionsDb};
+    std::unique_ptr<WordDescriptionServiceMock> wordDescriptionServiceInit =
+        std::make_unique<StrictMock<WordDescriptionServiceMock>>();
+    WordDescriptionServiceMock* wordDescriptionService =
+        wordDescriptionServiceInit.get();
+    WordDescriptionGeneratorImpl generator{
+        std::move(wordDescriptionServiceInit)};
 };
+
+TEST_F(
+    WordDescriptionGeneratorImplTest,
+    givenEnglishWordAndServiceDoesNotContainWordDescription_shouldReturnWordDescriptionWithNoDescription)
+{
+    EXPECT_CALL(*wordDescriptionService, getWordDescription(englishWord1))
+        .WillOnce(Return(boost::none));
+
+    const auto actualWordDescription =
+        generator.generateWordDescription(englishWord1);
+
+    ASSERT_EQ(actualWordDescription, wordDescription1);
+}
+
+TEST_F(
+    WordDescriptionGeneratorImplTest,
+    givenEnglishWordAndServiceContainsWordDescription_shouldReturnWordDescription)
+{
+    EXPECT_CALL(*wordDescriptionService, getWordDescription(englishWord2))
+        .WillOnce(Return(wordDescription2Opt));
+
+    const auto actualWordDescription =
+        generator.generateWordDescription(englishWord2);
+
+    ASSERT_EQ(actualWordDescription, wordDescription2);
+}
+
+TEST_F(
+    WordDescriptionGeneratorImplTest,
+    givenEnglishWords_shouldGenerateAmountOfWordsDescriptionsEqualAmountOfEnglishWords)
+{
+    EXPECT_CALL(*wordDescriptionService, getWordDescription(englishWord1))
+        .WillOnce(Return(wordDescription1Opt));
+    EXPECT_CALL(*wordDescriptionService, getWordDescription(englishWord2))
+        .WillOnce(Return(wordDescription2Opt));
+    EXPECT_CALL(*wordDescriptionService, getWordDescription(englishWord3))
+        .WillOnce(Return(boost::none));
+
+    const auto actualWordsDescriptions =
+        generator.generateWordsDescriptions(englishWords);
+
+    ASSERT_EQ(actualWordsDescriptions, wordsDescriptions);
+}
