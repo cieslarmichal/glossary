91509ae2860f086ed80ea9ac277d37ced6bc5fc4
michalovsky <michalovskyyy@gmail.com>
2020-02-27 23:37:45 +0100

    add script for running UTs

---
+++ runUT_linux.sh
@@ -0,0 +1,10 @@
+env:
+C_COMPILER=gcc-8
+CXX_COMPILER=g++-8
+
+mkdir build
+cd build
+env CC=${C_COMPILER} CXX=${CXX_COMPILER} cmake .. -DENABLE_COVERAGE:BOOL=TRUE -DCMAKE_BUILD_TYPE=DEBUG
+echo "We have" `nproc` "cores here"
+make -j `nproc`
+ctest -j `nproc` -V
