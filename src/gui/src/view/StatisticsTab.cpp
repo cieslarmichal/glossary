b4afe9fa4e7f358098eac32d2de8a0345f3b8293
michalovsky <michalovskyyy@gmail.com>
2020-06-03 23:30:42 +0200

    connect statistcs tab with glossary adapter

--- src/gui/src/view/StatisticsTab.cpp
+++ src/gui/src/view/StatisticsTab.cpp
@@ -12,12 +12,62 @@ namespace glossary::gui::view
 StatisticsTab::StatisticsTab(QWidget* parent) : QChartView(parent), ui(new Ui::StatisticsTab)
 {
     ui->setupUi(this);
+    ui->listOfDictionaries->setModel(&dictionaryNames);
     chartView= new QChartView{this};
+    chartView->setRenderHint(QPainter::Antialiasing);
+}
 
+StatisticsTab::~StatisticsTab()
+{
+    delete chartView;
+    delete ui;
+}
+
+void StatisticsTab::setDictionariesStatistics(const DictionariesStatistics & dictionariesStatisticsInit)
+{
+    dictionariesStatistics = dictionariesStatisticsInit;
+
+    QStringList dictionaryNamesFromStatistics;
+    for(const auto& dictionaryStatistics : dictionariesStatistics)
+    {
+        dictionaryNamesFromStatistics.push_back(QString::fromStdString(dictionaryStatistics.dictionaryName));
+    }
+    dictionaryNames.setStringList(dictionaryNamesFromStatistics);
+}
+
+void StatisticsTab::onDictionaryStatisticsReceived(const DictionaryStatistics&)
+{
+
+}
+
+void StatisticsTab::onDictionariesStatisticsReceived(const DictionariesStatistics& dictionaryStatistics)
+{
+    setDictionariesStatistics(dictionaryStatistics);
+}
+
+void StatisticsTab::on_listOfDictionaries_clicked(const QModelIndex &dictionaryNameIndex)
+{
+    QString dictionaryName = dictionaryNameIndex.data(Qt::DisplayRole).toString();
+
+    auto dictionaryStatistics = getDictionaryStatistics(dictionaryName);
+
+    updateCurrentDictionaryStatistics(dictionaryStatistics);
+}
+
+void StatisticsTab::updateCurrentDictionaryStatistics(const DictionaryStatistics & dictionaryStatistics)
+{
+    ui->editCorrectAnswers->setText(QString::fromStdString(std::to_string(dictionaryStatistics.correctAnswers)));
+    ui->editIncorrectAnswers->setText(QString::fromStdString(std::to_string(dictionaryStatistics.incorrectAnswers)));
+    double efficiency = dictionaryStatistics.correctAnswers / dictionaryStatistics.incorrectAnswers;
+    ui->editEfficiency->setText(QString::fromStdString(std::to_string(efficiency)));
+    updateChartView(dictionaryStatistics);
+}
+
+void StatisticsTab::updateChartView(const DictionaryStatistics &)
+{
     QChart* chart = new QChart();
     setChart(chart);
     chartView->setChart(chart);
-
     ui->layoutChartView->addWidget(chartView,0,0);
 
     QPieSeries * series = new QPieSeries();
@@ -26,35 +76,33 @@ StatisticsTab::StatisticsTab(QWidget* parent) : QChartView(parent), ui(new Ui::S
     series->append("Incorrect answers", .40);
 
     QPieSlice * slice = series->slices().at(0);
-//    slice->setLabelVisible();
     slice->setLabel("Correct answers");
     slice->setPen(QPen(Qt::darkGreen, 2));
     slice->setBrush(Qt::green);
 
     QPieSlice *slice1 = series->slices().at(1);
-//    slice1->setLabelVisible();
     slice1->setLabel("Incorrect answers");
-//    slice1->setLabel(QString("%1%").arg(100*slice1->percentage(), 0, 'f', 1));
     slice1->setPen(QPen(Qt::darkRed, 2));
     slice1->setBrush(Qt::red);
 
-//    series->setLabelsVisible();
     series->setLabelsPosition(QPieSlice::LabelOutside);
 
     chart->addSeries(series);
     chart->setTitle("Dictionary statistics");
-//    chart->legend()->hide();
 
-    chartView->setRenderHint(QPainter::Antialiasing);
     chartView->show();
 }
 
-StatisticsTab::~StatisticsTab()
+DictionaryStatistics StatisticsTab::getDictionaryStatistics(const QString &dictionaryName) const
 {
-    delete ui;
+    auto foundDictionarStatistics =
+        std::find_if(dictionariesStatistics.begin(), dictionariesStatistics.end(),
+                     [&](const DictionaryStatistics& stats) { return stats.dictionaryName == dictionaryName.toStdString(); });
+    if (foundDictionarStatistics != dictionariesStatistics.end())
+    {
+        return *foundDictionarStatistics;
+    }
+    return {};
 }
 
-void StatisticsTab::onDictionaryStatisticsReceived(const DictionaryStatistics&) {}
-
-void StatisticsTab::onDictionariesStatisticsReceived(const DictionariesStatistics&) {}
 }
--- src/gui/src/view/StatisticsTab.h
+++ src/gui/src/view/StatisticsTab.h
@@ -2,6 +2,7 @@
 
 #include <QChartView>
 #include <QWidget>
+#include <QStringListModel>
 
 #include "DictionaryStatistics.h"
 QT_CHARTS_USE_NAMESPACE
@@ -21,7 +22,7 @@ public:
     explicit StatisticsTab(QWidget* parent = nullptr);
     ~StatisticsTab();
 
-    //    void setAvailableLanguages(const QStringList&);
+    void setDictionariesStatistics(const DictionariesStatistics&);
 
 signals:
     void notifyAboutDictionaryStatisticsRequest(const QString& dictionaryName) const;
@@ -31,9 +32,18 @@ public slots:
     void onDictionaryStatisticsReceived(const DictionaryStatistics&);
     void onDictionariesStatisticsReceived(const DictionariesStatistics&);
 
-private:
-    Ui::StatisticsTab* ui;
+private slots:
+    void on_listOfDictionaries_clicked(const QModelIndex& dictionaryNameIndex);
 
+private:
+    void updateCurrentDictionaryStatistics(const DictionaryStatistics&);
+    void updateChartView(const DictionaryStatistics&);
+    DictionaryStatistics getDictionaryStatistics(const QString& dictionaryName) const;
+
+    DictionariesStatistics dictionariesStatistics;
+    QStringListModel dictionaryNames;
+
+    Ui::StatisticsTab* ui;
     QChartView* chartView;
 };
 }
